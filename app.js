cbfac77e257bdbd5330b23b134e11b4595ceca51
Kota <thomaschendev0412@gmail.com>
2024-05-14 22:08:46 +0900

    fix0514

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors'
+import authController from './controllers/authController.js';
 import userController from './controllers/userController.js';
 import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(userController);
+app.use('/auth', authController);
+app.use('/user', userController);
 
 mongoose.connect('mongodb://localhost:27017/inspect_db', {
     // useNewUrlParser: true,
