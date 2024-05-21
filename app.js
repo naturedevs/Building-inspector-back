6ca92e20e8d856d4cac350790de0b56d767acaae
Kota <thomaschendev0412@gmail.com>
2024-05-21 12:39:56 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import authController from './controllers/authController.js';
 import userController from './controllers/userController.js';
 import roleController from './controllers/roleController.js';
+import frmController from './controllers/frmController.js';
 import mongoose from 'mongoose';
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 app.use('/auth', authController);
 app.use('/users', userController);
 app.use('/roles', roleController);
+app.use('/frms', frmController);
 
 mongoose.connect('mongodb://localhost:27017/inspect_db', {
     // useNewUrlParser: true,
